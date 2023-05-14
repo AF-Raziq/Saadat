c6f13b7b0f8bc1583cddb91ba8c9783317d9bfb8
AF-Raziq <133484045+AF-Raziq@users.noreply.github.com>
2023-05-14 08:27:31 +0430

    Add files via upload

--- Saadat.py
+++ Saadat.py
@@ -947,7 +947,7 @@ def menu_apikey():
 ==================================================   """)
 
 def Subscraption():
-	key1=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
+	key1=open('/data/data/com.termux/files/usr/bin/.AFGHAN-cov', 'r').read()
 	clear()
 	print(logo)
 	r1=requests.get("https://pastebin.com/p3jbWM14").text
