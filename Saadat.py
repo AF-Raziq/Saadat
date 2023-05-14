6c3629211fac122359235b5d52cbaec8309f0507
AF-Raziq <133484045+AF-Raziq@users.noreply.github.com>
2023-05-14 08:19:54 +0430

    Add files via upload

--- Saadat.py
+++ Saadat.py
@@ -970,7 +970,7 @@ def Subscraption():
 		print("")
 		print(" Copy And Send Key To Admin")
 		print ("")
-		print (" Your Key : "RS+key1)
+		print (" Your Key : "+RS+key1)
 		print ("")
 		name = input(" Your Name : "Saadat)
 		print ("")
