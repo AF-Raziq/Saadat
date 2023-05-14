db6d8a6cd9ad51669afaab82aff7088639cf69ae
AF-Raziq <133484045+AF-Raziq@users.noreply.github.com>
2023-05-14 08:25:09 +0430

    Add files via upload

--- Saadat.py
+++ Saadat.py
@@ -974,7 +974,7 @@ def Subscraption():
 		print ("")
 		name = input(" Your Name : "+Saadat)
 		print ("")
-		input(" Press Enter To Send Key"RS)
+		input(" Press Enter To Send Key"+RS)
 		time.sleep(3.5)
 		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
 		os.system('am start https://wa.me/+93702856593?text=' + tks)
